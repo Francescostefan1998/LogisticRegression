1c8bdc0ae2719865786327d554ff0fb05d7a0c0c
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2024-12-30 17:45:34 +0100

    train an SVM model to classify the different flowers in the Iris dataset

--- useLogisticRegression.py
+++ useLogisticRegression.py
@@ -127,8 +127,10 @@ plt.tight_layout()
 plt.show()
 
 weights, params = [], []
+# the following c parameter as it increse it should decrease the strenght of the regularization function
 for c in np.arange(-5, 5):
     lr = LogisticRegression(C=10.**c, multi_class='ovr')
+    print(f'C value : {10.**c}')
     lr.fit(X_train_std, y_train)
     weights.append(lr.coef_[1])
     params.append(10.**c)
@@ -140,4 +142,15 @@ plt.ylabel('Weight coefficient')
 plt.xlabel('C')
 plt.legend(loc='upper left')
 plt.xscale('log')
+plt.show()
+
+
+from sklearn.svm import SVC
+svm = SVC(kernel = 'linear', C=1.0, random_state=1)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('Petal length [standardized]')
+plt.ylabel('Petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
 plt.show()
