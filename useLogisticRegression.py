57992d8016b121bd5b5495e321d3dcd4cdc2e60d
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2024-12-31 18:54:30 +0100

    using the kernel trick to create the separation decision boundry and play with gamma to increase the precision

--- useLogisticRegression.py
+++ useLogisticRegression.py
@@ -144,8 +144,7 @@ y_combined = np.hstack((y_train, y_test))
 # plt.xscale('log')
 # plt.show()
 
-
-# from sklearn.svm import SVC
+from sklearn.svm import SVC
 # svm = SVC(kernel = 'linear', C=1.0, random_state=1)
 # svm.fit(X_train_std, y_train)
 # plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
@@ -181,3 +180,29 @@ plt.ylabel('Feature 2')
 plt.legend(loc='best')
 plt.tight_layout()
 plt.show()
+
+svm = SVC(kernel='rbf', random_state=1, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+svm = SVC(kernel='rbf', random_state=1, gamma=0.2, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('Petal length [standardized]')
+plt.ylabel('Petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+svm = SVC(kernel='rbf', random_state=1, gamma=100.0, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('Petal length [standardized]')
+plt.ylabel('Petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
