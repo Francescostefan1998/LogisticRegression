140a4a43f191c2d659c2840dd0dae5967fc854ae
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-01-06 13:17:23 +0100

    plotting a decision tree

--- useLogisticRegression.py
+++ useLogisticRegression.py
@@ -217,3 +217,15 @@ plt.show()
 # plt.show()
 
 
+
+from sklearn.tree import DecisionTreeClassifier
+tree_model = DecisionTreeClassifier(criterion='gini', max_depth=4, random_state=1)
+tree_model.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier=tree_model, test_idx=range(105, 150))
+plt.xlabel('Petal length [cm]')
+plt.ylabel('Petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
